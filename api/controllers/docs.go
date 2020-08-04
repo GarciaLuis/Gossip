17496f644776e1980ab20b4c867e51a4d279dee1
garcialuis <garcialuisdev@gmail.com>
2020-08-04 15:31:24 -0700

    + Added swagger annotations to Post model and related endpoints, also annotated Token model

--- api/controllers/docs.go
+++ api/controllers/docs.go
@@ -28,6 +28,8 @@ package controllers
 
 import "github.com/garcialuis/Gossip/api/models"
 
+// MODELS USED TO DOCUMENT USER ENDPOINTS:
+
 // User record
 // swagger:response userResponse
 type userResponseWrapper struct {
@@ -50,4 +52,22 @@ type usersResponseWrapper struct {
 	// All current users
 	// in: body
 	Body []models.User
+}
+
+// MODELS USED TO DOCUMENT POST ENDPOINTS:
+
+// Post model
+// swagger:response postResponse
+type postResponseWrapper struct {
+	// Post record
+	// in: body
+	Body models.Post
+}
+
+// List of Posts
+// swagger:response postsResponse
+type postsResponseWrapper struct {
+	// List of post records
+	// in: body
+	Body []models.Post
 }
